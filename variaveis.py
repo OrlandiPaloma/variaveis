0a5ec49d85a5532eddf39e9b726c054b8af390cb
Ubiratan <palomaorlandiw@gmail.com>
2022-06-06 20:16:19 -0300

    variaveis

---
+++ variaveis.py
@@ -0,0 +1,16 @@
+'''
+# variaveis
+# números
+velocidade_internet = 10
+print(velocidade_internet)
+nota_filme = 8.5 #float saovaloresdecimais
+# valores boleanos
+esta_aberto = True #True or False
+# Strings
+nome_do_curso = "Lógica de Programação"
+'''
+
+salario_mensal = input("qual é o eu salario mensal?")
+horas_trabalhadas_por_mes = input("quantas horas trabalha por mes?")
+valor_hora = int(salario_mensal) / int(horas_trabalhadas_por_mes)
+print (valor_hora)
